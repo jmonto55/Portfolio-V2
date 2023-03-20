d402aec87fd674440830bcb9ccb8456ef0a7bc77
jmonto55 <jmonto55@gmail.com>
2023-03-19 20:02:33 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Router } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
 const App = () => {
 
   return (
-    <div className="App">
-      3D Developer Portfolio
-    </div>
+    <Router>
+      <div className="relative z-0 bg-primary">
+        3D Developer Portfolio
+      </div>
+    </Router>
   )
 };
 
