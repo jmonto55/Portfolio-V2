58f13c814f17f7da1f86783ebb3f789172ef7b1f
jmonto55 <jmonto55@gmail.com>
2023-03-20 17:26:39 -0500

    Build about cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{
                 y: [0, 24, 0]
               }}
@@ -43,4 +43,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
