d2dbf4afcd86ed2af8d434e569bdecb000b15b2a
jmonto55 <jmonto55@gmail.com>
2023-03-22 18:25:35 -0500

    Add media query for skills logos

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,51 @@
+import { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech, index) => (
-        <div className="w-28 h-28" key={tech.name}>
-          <BallCanvas icon={tech.icon} />
-        </div>
-      ))}
-    </div>
-  )
-}
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery1 = window.matchMedia("(max-width: 768px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery1.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery1.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery1.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((tech, index) => (
+          <img className="h-16 w-16" src={tech.icon} alt={tech.name} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((tech, index) => (
+          <div className="w-28 h-28" key={tech.name}>
+            <BallCanvas icon={tech.icon} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default SectionWrapper(Tech, '');
