9e1337abf837040c6c1a114ffd7ab4b659c658c6
jmonto55 <jmonto55@gmail.com>
2023-03-21 23:35:24 -0500

    Set state and event listeners

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="">
           <Navbar />
           <Hero />
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,9 +18,6 @@ module.exports = {
       screens: {
         xs: "450px",
       },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
-      },
     },
   },
   plugins: [],
