a75b4fa6379b0941d2a6f9122f090306943d602e
jmonto55 <jmonto55@gmail.com>
2023-03-22 12:21:04 -0500

    fix animations

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,23 +1,25 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-import { styles } from "../styles"
-import { staggerContainer } from "../utils/motion"
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
-function HOC() {
-  return (
-    <motion.section
-      variants={staggerContainer()}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true, amnount: 0.25 }}
-      className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-    >
-      <span className="hash-span" id={idName}>&nbsp;</span>
-      <Component />
-    </motion.section>
-  )
-} 
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className='hash-span' id={idName}>
+          &nbsp;
+        </span>
 
+        <Component />
+      </motion.section>
+    );
+  };
 
 export default SectionWrapper;
