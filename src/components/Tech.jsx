0b9d41f7d5bb142d99a5efdc4b85953367a61210
jmonto55 <jmonto55@gmail.com>
2023-03-22 12:35:43 -0500

    fix animations

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((tech, index) => (
         <div className="w-28 h-28" key={tech.name}>
-          <BallCanvas icon={tech.icon} />
+          <div icon={tech.icon} />
         </div>
       ))}
     </div>
