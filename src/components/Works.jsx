3f64cd88f1dc7a87840b047f0042f1db83cfdc25
jmonto55 <jmonto55@gmail.com>
2023-03-23 12:31:12 -0500

    Fix Next logo

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import Tilt from "react-tilt"
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 const Works = () => {
   return (
     <div>Works</div>
-  )
-}
+  );
+};
 
 export default Works
