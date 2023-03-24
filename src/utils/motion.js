ae39c65469b91dc1fc17cf25d235175f73e155dd
jmonto55 <jmonto55@gmail.com>
2023-03-24 18:23:50 -0500

    Increase animation performance

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -9,7 +9,7 @@ export const textVariant = (delay) => {
       opacity: 1,
       transition: {
         type: "spring",
-        duration: 1.25,
+        duration: 1,
         delay: delay,
       },
     },
