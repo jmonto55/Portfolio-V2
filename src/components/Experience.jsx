3575fe970151e39a4158435d584e5dfb6a736502
jmonto55 <jmonto55@gmail.com>
2023-03-24 18:34:22 -0500

    Increase animation performance

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,7 @@ const Experience = () => {
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
-      <div className="mt-20 flex flex-col">
+      <div className="mt-10 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience={experience} />
